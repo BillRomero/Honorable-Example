40b2e603a5da3dc70704d13a28868adb824c908c
Jack Romero <romerojackbill@gmail.com>
2023-06-12 20:50:07 -0500

    fibonacci exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var jack = 14
+    var kenny = 27
+    var gloria:Long = 52
+    var entero = 100
+    var boleano = jack < kenny
 
-    println("Program arguments: ${args.joinToString()}")
+    var edad = jack + kenny
+    edad += gloria.toInt()
+
+    var lista = arrayOf(2, 4, 5, 7, 8)
+
+
+
+
+    var fibonacci:Int = 0  //  5
+    var antes = 1 // 2
+    var result: Int //  3
+    for (i in 1..100){
+        result = antes + fibonacci
+        antes = fibonacci
+        fibonacci = result
+        println(fibonacci)
+    }
 }
